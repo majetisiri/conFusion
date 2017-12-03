8e27c8e3b0b45b40ad0f57b841a5f4186785ceb8
Srividya Majeti <srividya.majeti@commercialwebservices.com>
2017-12-02 23:58:17 -0500

    master:  factories, services and templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports= function(grunt){
 		}, 
 
 		useminPrepare: {
-		  html: 'app/menu.html',
+		  html: 'app/index.html',
 		  options: {
 		    dest: 'dist'
 		  }
@@ -156,7 +156,7 @@ module.exports= function(grunt){
 		      base:{
 		        path: 'dist',
 		        options: {
-		          index: 'menu.html',
+		          index: 'index.html',
 		          maxAge: 300000
 		        }
 		      }
